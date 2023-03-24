735b5472bf7c7ef1f3bc21b05d3be015762be4c1
pedro558 <pedroafonso109@gmail.com>
2023-03-24 15:13:48 -0300

    :sparkles: add section container

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,12 +1,20 @@
 import './styles.js'
-import { Container } from './styles'
+import { Container, Links } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
+import { Section } from '../../components/Section'
 
 export function Details(){
   return(
     <Container>
       <Header></Header>
+      <Section title='Links úteis'>
+        <Links>
+          <li><a href="#">Item x</a></li>
+          <li><a href="#">Item x</a></li>
+          <li><a href="#">Item x</a></li>
+        </Links>
+      </Section>
       <Button title = 'Voltar' loading></Button>
       
     </Container>

523ac803074607ca4b53b86ba0f919745fcfe710
pedro558 <pedroafonso109@gmail.com>
2023-03-24 16:54:10 -0300

    :lipstick: finishing styles details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,5 @@
 import './styles.js'
-import { Container, Links } from './styles'
+import { Container, Links, Content } from './styles'
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
@@ -10,23 +10,34 @@ export function Details(){
   return(
     <Container>
       <Header/>
-      <ButtonText title='Excluir nota'/>
 
-      <Section title='Links úteis'>
-        <Links>
-          <li><a href="#">Item x</a></li>
-          <li><a href="#">Item x</a></li>
-          <li><a href="#">Item x</a></li>
-        </Links>
-      </Section>
+      <main>
+      <Content>
+        <ButtonText title='Excluir nota'/>
 
-      <Section title='Marcadores'>
-        <Tag title='express'/>
-        <Tag title='node'/>
-      </Section>
+        <h1>Introdução ao React</h1>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, omnis! Architecto deleniti quos qui obcaecati voluptates excepturi quasi ratione modi pariatur ut aut officiis totam exercitationem laboriosam corporis, est iusto!
+        Repellat, quas! Amet id ipsam expedita exercitationem numquam quae odio rem sit accusamus nulla ducimus voluptatum, velit, fugiat a vel voluptates nisi iure excepturi ipsa quia ea animi sunt? Sint.
+        Provident eaque itaque quasi! Pariatur saepe in consequuntur vero, atque nobis cupiditate repellendus soluta earum similique odio! Consequatur saepe libero modi tempore consequuntur, dolor, dolorem doloribus non tempora, commodi quisquam!
+        Totam veritatis, doloremque autem quos quaerat sed accusantium soluta facere voluptatibus corrupti officia maiores aspernatur iusto est qui error aut velit placeat at rerum consequatur unde? Fugit veniam officiis suscipit?
+        Deserunt hic quo reiciendis itaque, expedita dignissimos magnam fuga fugit perferendis quod vero, laudantium cumque commodi atque cum? Voluptatum dolores non ex cum quidem atque eos eligendi, tempora voluptatem laborum!
+        </p>
 
-      <Button title = 'Voltar' loading></Button>
-      
+        <Section title='Links úteis'>
+          <Links>
+            <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            <li><a href="#">https://www.rocketseat.com.br/</a></li>
+          </Links>
+        </Section>
+
+        <Section title='Marcadores'>
+          <Tag title='express'/>
+          <Tag title='node'/>
+        </Section>
+
+        <Button title = 'Voltar' loading></Button>
+      </Content>
+      </main>
     </Container>
   )
 }

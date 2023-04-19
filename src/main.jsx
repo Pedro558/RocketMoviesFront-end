f5c094533950fe41ef04258622672d33f721a3d6
pedro558 <pedroafonso109@gmail.com>
2023-04-19 12:43:29 -0300

    :sparkles: feat: add NewNotes page

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-import { Profile } from './pages/Profile'
+import { New } from './pages/New'
 
 import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
@@ -13,7 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <Profile />
+      <New />
     </ThemeProvider>
   </React.StrictMode>,
 )

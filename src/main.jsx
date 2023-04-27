7836fdacfe45fc8b50589e960ac695c18e27c259
pedro558 <pedroafonso109@gmail.com>
2023-04-27 12:53:30 -0300

    :sparkles: add routes to main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { New } from './pages/New'
-
-import { ThemeProvider } from 'styled-components'
 import GlobalStyles from './styles/global'
+import { ThemeProvider } from 'styled-components'
+
 import theme from './styles/theme'
 
+import { Routes } from './routes'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <New />
+      <Routes />
     </ThemeProvider>
   </React.StrictMode>,
 )

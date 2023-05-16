619b0446664171adf4b2bcb2f97907f181ef68d7
pedro558 <pedroafonso109@gmail.com>
2023-05-16 11:14:31 -0300

    :sparkles: adding state to variables

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import { Background, Container, Form } from './styles'
 import { FiUser, FiMail, FiLock } from 'react-icons/fi'
 
@@ -8,6 +9,16 @@ import { Link } from 'react-router-dom'
 
 
 export function SignUp(){
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+
+  function handleSignUp(){
+    if(!name || !email || !password){
+      return alert('Preencha todos os campos')
+    }
+  }
+
   return (
     <Container>
 
@@ -23,21 +34,24 @@ export function SignUp(){
           placeholder="Nome"
           type="text"
           icon={FiUser}
+          onChange={e => setName(e.target.value)}
         />
 
         <Input
           placeholder="E-mail"
           type="email"
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
 
         <Input
           placeholder="Senha"
           type="password"
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title='Cadastrar'/>
+        <Button title='Cadastrar' onClick={handleSignUp}/>
 
         <Link to="/">Voltar para o Login</Link>
 

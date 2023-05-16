e4fab983d9318242abfb52ce9c70f46872a0aa3b
pedro558 <pedroafonso109@gmail.com>
2023-05-16 12:01:22 -0300

    :sparkles: add useNavigate

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -6,13 +6,15 @@ import { FiUser, FiMail, FiLock } from 'react-icons/fi'
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
 
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 
 export function SignUp(){
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+
+  const navigate = useNavigate()
 
   function handleSignUp(){
     if(!name || !email || !password){
@@ -22,6 +24,7 @@ export function SignUp(){
     api.post('/users', {name, email, password})
     .then(() =>{
       alert('Usuário cadastrado com sucesso!')
+      navigate('/')
     })
     .catch( error =>{
       if(error.response){

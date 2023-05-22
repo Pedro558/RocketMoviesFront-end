a36303bc63684529e41d6fb2077d126923d98536
pedro558 <pedroafonso109@gmail.com>
2023-05-22 09:56:42 -0300

    :recycle: refactor: default config token with axios

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -16,7 +16,7 @@ function AuthProvider({ children }){
       localStorage.setItem('@rocketnotes:user', JSON.stringify(user));
       localStorage.setItem('@rocketnotes:token', token);
 
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
       setData({ user, token })
       
       console.log(user, token);
@@ -44,7 +44,8 @@ function AuthProvider({ children }){
     const user = localStorage.getItem("@rocketnotes:user")
 
     if(token && user){
-      api.defaults.headers.authorization = `Bearer ${token}`
+      api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+      setData({ user, token })
 
       setData({
         token,

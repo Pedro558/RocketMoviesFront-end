aa2f069afd70f801d9bbef4d6d6e4b63f7399456
pedro558 <pedroafonso109@gmail.com>
2023-05-26 14:03:49 -0300

    :recycle: refactor: header avatar img

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,17 +1,21 @@
 import { Container, Profile, Logout } from './styles'
 import { RiShutDownLine } from 'react-icons/ri'
 
+import { api } from '../../services/api'
+
 import { useAuth } from '../../hooks/auth'
 
 export function Header(){
 
-  const { signOut } = useAuth();
+  const { signOut, user } = useAuth();
+
+  const avatarUrl = user.avatar ? `${api.defaults.baseURL}/files/${user.avatar}` : avatarPlaceholder
 
   return(
     <Container>
       <Profile to='/profile'>
         <img
-          src='https://github.com/Pedro558.png'
+          src={avatarUrl}
           alt='Foto de perfil do usuário'
         />
 

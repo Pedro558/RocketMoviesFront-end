fb79449e8caca679113c5c663aede085053ff383
pedro558 <pedroafonso109@gmail.com>
2023-06-02 09:30:34 -0300

    :recycle: refactor: get details data from the server

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,5 +1,8 @@
-import './styles.js'
+import { useState, useEffect } from 'react'
+import { useParams, useNavigate } from 'react-router-dom'
+import { api } from '../../services/api'
 import { Container, Links, Content } from './styles'
+
 import { Button } from '../../components/Button'
 import { Header } from '../../components/Header'
 import { Section } from '../../components/Section'
@@ -7,37 +10,89 @@ import { Tag } from '../../components/Tag'
 import { ButtonText } from '../../components/ButtonText'
 
 export function Details(){
+  const params = useParams()
+  const navigate = useNavigate()
+
+  const [data, setData] = useState(null)
+
+  useEffect(() =>{
+    async function fetchNote(){
+      const response = await api.get(`/notes/${params.id}`)
+      console.log(data);
+      setData(response.data)
+    }
+
+    fetchNote()
+    
+  }, [])
+
+  function handleBack(){
+    navigate('/')
+  }
+
+  async function handleRemove(){
+    const confirm = window.confirm('Deseja realmente remover a nota ?')
+
+    if(confirm){
+      await api.delete(`/notes/${params.id}`)
+      navigate('/')
+    }
+  }
+
   return(
     <Container>
       <Header/>
 
+    {
+      data &&
       <main>
       <Content>
-        <ButtonText title='Excluir nota'/>
+        <ButtonText
+         title='Excluir nota'
+         onClick={handleRemove}
+         />
 
-        <h1>Introdução ao React</h1>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eligendi, omnis! Architecto deleniti quos qui obcaecati voluptates excepturi quasi ratione modi pariatur ut aut officiis totam exercitationem laboriosam corporis, est iusto!
-        Repellat, quas! Amet id ipsam expedita exercitationem numquam quae odio rem sit accusamus nulla ducimus voluptatum, velit, fugiat a vel voluptates nisi iure excepturi ipsa quia ea animi sunt? Sint.
-        Provident eaque itaque quasi! Pariatur saepe in consequuntur vero, atque nobis cupiditate repellendus soluta earum similique odio! Consequatur saepe libero modi tempore consequuntur, dolor, dolorem doloribus non tempora, commodi quisquam!
-        Totam veritatis, doloremque autem quos quaerat sed accusantium soluta facere voluptatibus corrupti officia maiores aspernatur iusto est qui error aut velit placeat at rerum consequatur unde? Fugit veniam officiis suscipit?
-        Deserunt hic quo reiciendis itaque, expedita dignissimos magnam fuga fugit perferendis quod vero, laudantium cumque commodi atque cum? Voluptatum dolores non ex cum quidem atque eos eligendi, tempora voluptatem laborum!
-        </p>
+        <h1>{data.title}</h1>
+        <p>{data.description}</p>
 
+      {
+        data.links &&
         <Section title='Links úteis'>
           <Links>
-            <li><a href="#">https://www.rocketseat.com.br/</a></li>
-            <li><a href="#">https://www.rocketseat.com.br/</a></li>
+            {
+              data.links.map(link => (
+                <li key={link.id}>
+                  <a href={link.url} target='_blank'>
+                  {link.url}
+                  </a>
+                </li>
+              ))
+            }
           </Links>
         </Section>
+      }
 
-        <Section title='Marcadores'>
-          <Tag title='express'/>
-          <Tag title='node'/>
-        </Section>
+        {
+          data.tags &&
+          <Section title='Marcadores'>
+            {
+              data.tags.map(tag => (
+                <Tag 
+                  key={tag.id}
+                  title={tag.name}
+                />
+              ))
+            }
+          </Section>
+        }
 
-        <Button title = 'Voltar' loading></Button>
+        <Button 
+          title = 'Voltar' 
+          onClick={handleBack}
+        />
       </Content>
       </main>
+    }
     </Container>
   )
 }
